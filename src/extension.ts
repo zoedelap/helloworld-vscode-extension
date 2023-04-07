d8dc4ba8154c3124aeb2ecb61e39259e7242fd03
zoedelap <zoe.a.delap@gmail.com>
2023-04-07 15:03:16 -0500

    changed webview to displaying in the explorer

--- src/extension.ts
+++ src/extension.ts
@@ -2,34 +2,46 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log("Hello world is now running! Good job!");
-	let disposable = vscode.commands.registerCommand('helloworld.start', () => {
-		// Create and show a new webview
-		const panel = vscode.window.createWebviewPanel(
-		  'helloworld', // Identifies the type of the webview. Used internally
-		  'Hello World', // Title of the panel displayed to the user
-		  vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-		  {} // Webview options. More on these later.
-		);
 
-		// set webview contents
-		panel.webview.html = getWebviewContent();
-	});
-	context.subscriptions.push(disposable);
+	const provider = new HelloWorldViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(HelloWorldViewProvider.viewType, provider));
 }
 
-function getWebviewContent() {
-	console.log("Getting webview contents");
-	return `<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Hello World</title>
-	</head>
-	<body>
-		<h1>Hello World!</h1>
-	</body>
-	</html>`;
+class HelloWorldViewProvider implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'helloworld.start';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		console.log("Getting webview contents");
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Hello World</title>
+			</head>
+			<body>
+				<h1>Hello World!</h1>
+			</body>
+			</html>`;
+	}
 }
 
 export function deactivate() {}
