360ede23e6d14c62089b84a021515ca58a99a386
zoedelap <zoe.a.delap@gmail.com>
2023-04-07 14:34:31 -0500

    create an empty webview in the explorer window

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,18 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello VS Code!');
-		vscode.window.setStatusBarMessage('Hello VS Code!', 3000);
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('helloworld.start', () => {
+		  // Create and show a new webview
+		  const panel = vscode.window.createWebviewPanel(
+			'helloworld', // Identifies the type of the webview. Used internally
+			'Hello World', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		  );
+		})
+	  );
+	// context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
